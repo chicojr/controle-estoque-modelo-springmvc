df9fd7e8e8f3c4a14c77b84f8e6417441d9ac18f
Roberto Alencar <roberto@tidius.com.br>
2016-03-08 10:02:49 -0300

    adicionando script sql

--- sql/script.sql
+++ sql/script.sql
@@ -25,3 +25,51 @@ CREATE TABLE usuario (
     login varchar(50),
     senha varchar(50)
 );
+
+
+-- -------------------------------------------------------------------------------------
+
+CREATE TABLE estado (
+
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	nome varchar(50),
+	sigla varchar(2),
+	habilitado boolean
+);
+
+CREATE TABLE cidade (
+
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	estado_id int,
+	nome  varchar(50),
+	habilitado boolean
+);
+
+CREATE TABLE empresa (
+
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	cidade_id int,
+	razao_social varchar(100),
+	nome_fantasia varchar(100),
+	cnpj varchar(18),
+	inscricao_estadual varchar(50),
+	website varchar(50),
+	rua varchar(100),
+	bairro varchar(50),
+	complemento varchar(100),
+	numero varchar(20),
+	cep varchar(10),
+	email varchar(50),
+	telefones varchar(100)
+);
+
+ALTER TABLE cidade ADD CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id) REFERENCES estado(id);
+ALTER TABLE empresa ADD CONSTRAINT fk_empresa_cidade FOREIGN KEY (cidade_id) REFERENCES cidade(id);
+
+
+
+
+
+
+
+
